10d19b003793edf19e5ce9be06f7b3e916a44bea
caseysnow <cesnow2@wpi.edu>
2022-09-09 10:41:26 -0400

    refactor: updated readme and hardcoded mac port

--- Primitives/ReplayPlay.py
+++ Primitives/ReplayPlay.py
@@ -13,6 +13,13 @@ from KoalbyHumanoid.robot import Robot
 NEXT STEPS:
 '''
 
+##Port Finder
+# import serial.tools.list_ports as ports
+#
+# com_ports = list(ports.comports())  # create a list of com ['COM1','COM2']
+# for i in com_ports:
+#     print(i.device)  # returns 'COMx'
+
 robot = Robot()
 replay = ReplayPrimitive(robot.motors)
 replay.poseTime = float(input("Enter pose time (seconds):")) + 0.005
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # koalby-humanoid
+
+Ports are different for Mac and Windows.
+With Koalby plugged in, go to ReplayPlay.py, uncomment the block of code labeled as port finder. Then, change the port to what gets printed.
+
